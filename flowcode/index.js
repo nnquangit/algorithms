7d48ff3ed43997290205bd13d155678ba0b1ee69
Quang <quang@Quangs-MacBook-Pro.local>
2022-07-23 13:52:52 +0700

    Add flowcode folder

--- flowcode/index.js
+++ flowcode/index.js
@@ -17,19 +17,19 @@ const loader = () =>
 //   clearInterval(loader);
 // }, 5000);
 
-
 Object.prototype.getPath = function (s) {
   const stringToObj = function (path, obj) {
-    var parts = path.split("."), part;
+    var parts = path.split("."),
+      part;
     var last = parts.pop();
-    while (part = parts.shift()) {
+    while ((part = parts.shift())) {
       if (typeof obj[part] != "object") obj[part] = {};
       obj = obj[part];
     }
     return obj[last];
-  }
+  };
   return s == "" ? this : stringToObj(s, this);
-}
+};
 
 const types = {
   excute: async (action, level, parent) => {
@@ -41,8 +41,8 @@ const types = {
       check: colours.red,
       action: colours.green,
       date: colours.yellow,
-      fetch: colours.yellow
-    }
+      fetch: colours.yellow,
+    };
 
     console.log(
       colorTypes[action.type] || colorTypes.default,
@@ -61,7 +61,7 @@ const types = {
     if (action.conditions) {
       await Promise.all(
         action.conditions.map(async (child) => {
-          isRun = isRun && await types.excute(child, level + 1, action);
+          isRun = isRun && (await types.excute(child, level + 1, action));
         })
       );
     }
@@ -80,7 +80,7 @@ const types = {
   action: async (action, level) => {
     await types.collectData(action, level);
 
-    if (!await types.isRunAble(action, level)) {
+    if (!(await types.isRunAble(action, level))) {
       return;
     }
 
@@ -102,7 +102,7 @@ const types = {
         return true;
         break;
       case "==":
-        return action.getPath(action.path) == action.value
+        return action.getPath(action.path) == action.value;
         break;
     }
     return false;
@@ -114,15 +114,46 @@ const tasks = {
   label: `Run Application`,
   type: "action",
   collect: [
-    { label: `Get mysql connection`, key: "date", type: "date", },
-    { label: `Get 3party connection`, key: "profile", type: "fetch", url: "https://reqres.in/api/users/1" },
-    { label: `Get socket connection`, key: "customer", type: "fetch", url: "https://reqres.in/api/users/2" },
-    { label: `Get odoo connection`, key: "user", type: "fetch", url: "https://reqres.in/api/users/3" },
-    { label: `Get queue connection`, key: "queue", type: "fetch", url: "https://reqres.in/api/users/4" },
-    { label: `Get elastic connection`, key: "queue", type: "fetch", url: "https://reqres.in/api/users/5" },
+    { label: `Get mysql connection`, key: "date", type: "date" },
+    {
+      label: `Get 3party connection`,
+      key: "profile",
+      type: "fetch",
+      url: "https://reqres.in/api/users/1",
+    },
+    {
+      label: `Get socket connection`,
+      key: "customer",
+      type: "fetch",
+      url: "https://reqres.in/api/users/2",
+    },
+    {
+      label: `Get odoo connection`,
+      key: "user",
+      type: "fetch",
+      url: "https://reqres.in/api/users/3",
+    },
+    {
+      label: `Get queue connection`,
+      key: "queue",
+      type: "fetch",
+      url: "https://reqres.in/api/users/4",
+    },
+    {
+      label: `Get elastic connection`,
+      key: "queue",
+      type: "fetch",
+      url: "https://reqres.in/api/users/5",
+    },
   ],
   conditions: [
-    { label: `Verify mysql connection`, type: "check", op: "==", path: "parent.data.profile.data.id", value: "1" },
+    {
+      label: `Verify mysql connection`,
+      type: "check",
+      op: "==",
+      path: "parent.data.profile.data.id",
+      value: "1",
+    },
     { label: `Verify 3party connection`, type: "check", op: "ignore" },
     { label: `Verify socket connection`, type: "check", op: "ignore" },
     { label: `Verify odoo connection`, type: "check", op: "ignore" },
@@ -131,10 +162,11 @@ const tasks = {
   ],
   actions: [
     {
-      label: `Watting for client`, type: "routes",
+      label: `Watting for client`,
+      type: "routes",
       routes: [
-        { url: '/user', label: `Watting for client`, type: "action", path: "" }
-      ]
+        { url: "/user", label: `Watting for client`, type: "action", path: "" },
+      ],
     },
   ],
 };
@@ -145,4 +177,4 @@ const devices = {
   TEMP_01: "46",
 };
 
-types.excute(tasks)
+types.excute(tasks);
